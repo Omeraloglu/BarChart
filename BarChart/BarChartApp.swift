ab670632c8ec9baf25bb0bc4636e25dd876f833f
Omer <omeraloglu16@hotmail.com>
2023-04-29 15:08:55 +0300

    Initial Commit

---
+++ BarChart/BarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartApp.swift
+//  BarChart
+//
+//  Created by Ömer ALOĞLU on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
